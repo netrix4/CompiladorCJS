5a36dcd751ebb847658a64eb6a67adce2563cbe2
Mario Arias <netrix4@gmail.com>
2023-11-07 22:35:45 -0800

    Final final practice 7, bro. tlj

--- ProyectoCompiladorAutomatasVanillaJs/app/Practica7.js
+++ ProyectoCompiladorAutomatasVanillaJs/app/Practica7.js
@@ -3,45 +3,31 @@ console.log("Editor/Compilador Online");
 // import * as diccionario from "../resources/DiccionarioCadenas.js";
 import {diccionario} from "../resources/DiccionarioCadenas.js";
 
-const cardContent = document.getElementById('cardContent')
+const filteredFileContent = document.getElementById('inputText')
 
 const selectedFile = document.getElementById('selectedFile')
 selectedFile.addEventListener('change', onSelectedFileChanged)
-
-const filteredFileContent = document.getElementById('inputText')
-// filteredFileContent.setAttribute('id', 'taFilteredFileContent')
-
 const downloadButton = document.getElementById('download')
 downloadButton.addEventListener('click', onDownload)
 
-downloadButton.textContent = 'Descargar'
-// cardContent.appendChild(filteredFileContent)
-// cardContent.appendChild(downloadButton)
-
-let fileReader;
-const findWordsRegex = /[0-9]|(\w)+|[\|&!]|((>=)|(<=)|(==))|[><]|[=*/%+\-]|[\(\){}\[\]]|[;]/gm
-
-let everyWordInFile;
-let analizedStrings;
-let filtredString;
-
 function onSelectedFileChanged() {
-    fileReader = new FileReader();
-    let originalFileString;
+    const fileReader = new FileReader();
     
     fileReader.readAsText(selectedFile.files[0])
     console.log('File readed: ', selectedFile.files[0].name)
     fileReader.onload = () => {
-        originalFileString = fileReader.result
+        const originalFileString = fileReader.result
         filteredFileContent.value = originalFileString
     }
 }
 function compileInputText() {
+    const findWordsRegex = /[0-9]|(\w)+|[\|&!]|((>=)|(<=)|(==))|[><]|[=*/%+\-]|[\(\){}\[\]]|[;]/gm
+
     const textAreaValue = document.getElementById('inputText').value
     let banderaNoEncontrado = true; 
 
-    everyWordInFile = textAreaValue.match(findWordsRegex)
-    analizedStrings = [];
+    const everyWordInFile = textAreaValue.match(findWordsRegex)
+    const analizedStrings = [];
 
     everyWordInFile.forEach((itemWordInFile) => {
         diccionario.forEach((ItemDiccionario) => {
@@ -59,19 +45,19 @@ function compileInputText() {
         banderaNoEncontrado = true
     });
 
-    filtredString = `Coicidencias encontradas: ${analizedStrings.length}\n`;
+    let filtredString = `Coicidencias encontradas: ${analizedStrings.length}\n`;
     analizedStrings.forEach((palabraAnalizada) => {
         const temp = `${palabraAnalizada[0]} - ${palabraAnalizada[1]}`;
         filtredString = filtredString.concat(temp, '\n')
     });
-        
+    return filtredString;
 }
 
 function onDownload() {
-    compileInputText();
+    const compiledOutputText = compileInputText();
 
     console.log('Archivo de salida listo para descargar');
-    const archivoBlob = new Blob([filtredString], { type: 'text/plain' });
+    const archivoBlob = new Blob([compiledOutputText], { type: 'text/plain' });
 
     const archivoURL = URL.createObjectURL(archivoBlob);
     
