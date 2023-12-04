507321785ff33d1aba09e05299861e6cad243ba6
Mario Arias <netrix4@gmail.com>
2023-12-03 17:25:11 -0800

    Second project depuration (remove unnecessary files)

--- antlr-sintactico/src/app/page.js
+++ antlr-sintactico/src/app/page.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 
 // Esta va ser la buena
 import {analizadorSintactico} from "../module/generador.js";
-// import { Result } from 'postcss';
 
 const HomePage = () => {
   const [inputUser, setInputUser] = useState('');
@@ -19,36 +18,32 @@ const HomePage = () => {
     const tempRes = []
     let resStr = 'Resultado(s):\n'
 
-    console.log('Operaciones identificadas: ' + rowsExpressions .length, rowsExpressions)
+    console.log('Operaciones identificadas: ' + rowsExpressions.length, rowsExpressions)
 
     rowsExpressions.forEach(expression => {
       let expresionTempRes = analizadorSintactico(expression)
       tempRes.push(' ' + expresionTempRes[expresionTempRes.length-1].toString())
       
     });
-    resStr += tempRes;
 
-    console.log('Resultados por operaciona antes del hook', resStr)
+    console.log('Resultados por operacion antes del hook', tempRes)
+    
+    resStr += tempRes;
     setResults(resStr)
-    console.log(results)
   }
 
   return (
-   
-      <div className='card-element-wrapper'>
-        <div className='card-element-header'>
-            <h2>Analizador Sintáctico</h2>
-        </div>
-        <div className='card-element-content' id="cardContent">
-          <p className='text-center' htmlFor="inputAutom">Ingresa tus operaciones aritméticas y haz click en el botón para ver el resultado en consola</p>
-          {/* <input type="file" id="selectedFile" accept=".txt" /> */}
-          <textarea id="inputText" value={inputUser} onChange={onTextInputChange}></textarea>
-          <button id="compile" onClick={onCompileClick}>Compilar</button>
-          <p>{results}
-          </p>
-        </div>
+    <div className='card-element-wrapper'>
+      <div className='card-element-header'>
+        <h2>Analizador Sintáctico</h2>
       </div>
- 
+      <div className='card-element-content' id="cardContent">
+        <p className='text-center' htmlFor="inputAutom">Ingresa tus operaciones aritméticas y haz click en el botón para conocer el resultado</p>
+        <textarea id="inputText" value={inputUser} onChange={onTextInputChange}></textarea>
+        <button id="compile" onClick={onCompileClick}>Compilar</button>
+        <p>{results}</p>
+      </div>
+    </div>
   )
 }
 
